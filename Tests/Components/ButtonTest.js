4056329a04074d5ee4e70caa25cf14919d9bc2af
arisupriatna14 <arisupriatna703@gmail.com>
2019-04-14 22:34:28 +0700

    test(Button): add new test

--- Tests/Components/ButtonTest.js
+++ Tests/Components/ButtonTest.js
@@ -29,4 +29,61 @@ test('Button component renders correctly if title, images, and onPress is set', 
     />
   ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+describe('Button Component', () => {
+  it('Will renders if title props is set', () => {
+    const testRenderer = renderer.create(<Button title="Example Title" />);
+    const testInstance = testRenderer.root;
+    expect(Object.keys(testInstance.props)).toEqual(['title']); // get props
+    expect(testInstance.props.title).toBe('Example Title'); // get value from props
+  });
+
+  it('Will renders if title and onPress is set', () => {
+    const onPress = () => {
+      console.log('Button Clicked');
+    }
+
+    const testRenderer = renderer.create(
+      <Button
+        title="Title"
+        onPress={onPress}
+      />
+    );
+
+    const testInstance = testRenderer.root;
+    const propsValue = Object.values(testInstance.props);
+
+    expect(propsValue[0]).toEqual('Title');
+    expect(propsValue[1] === onPress).toBeTruthy();
+    expect(Object.keys(testInstance.props)).toEqual(['title', 'onPress']);
+  });
+
+  it('Will renders if title, onPress, and icon button is set', () => {
+    const onPress = () => {
+      console.log('Button Clicked');
+    }
+
+    const testRenderer = renderer.create(
+      <Button
+        title="Title Button"
+        onPress={onPress}
+        sourceBtnImage={Images.icBack}
+      />
+    );
+
+    const testInstance = testRenderer.root;
+    const propsValue = Object.values(testInstance.props);
+
+    // check values from props
+    expect(propsValue[0]).toEqual('Title Button');
+    expect(propsValue[1] === onPress).toBeTruthy();
+    expect(propsValue[2]).toEqual({ testUri: '../../../app/Assets/Images/ic_back.png' });
+    // check props
+    expect(Object.keys(testInstance.props)).toEqual(['title', 'onPress', 'sourceBtnImage']);
+    // check validation prop types
+    expect(typeof testInstance.props.title).toBe('string');
+    expect(typeof testInstance.props.onPress).toBe('function');
+    expect(typeof testInstance.props.sourceBtnImage).toBe('object')
+  });
 })
