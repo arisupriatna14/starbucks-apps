6f775cf9d76149d9cfdc43f3553b43d7fa207ac8
arisupriatna14 <arisupriatna703@gmail.com>
2019-04-14 22:29:51 +0700

    feat(Checkout): add new screen Checkout

---
+++ app/Containers/Checkout/Checkout.style.js
@@ -0,0 +1,12 @@
+import { Colors } from '../../Themes/Colors';
+import { StylesGlobal } from '../../Themes/StyleGlobal';
+
+const Styles = {
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BLACK,
+    ...StylesGlobal.viewCenter
+  }
+};
+
+export { Styles };
---
+++ app/Containers/Checkout/Chekcout.screen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { View, Image } from 'react-native';
+import get from 'lodash/get';
+import { Transition } from 'react-navigation-fluid-transitions';
+
+import { Styles } from './Checkout.style';
+import { StylesGlobal } from '../../Themes/StyleGlobal';
+import { Images } from '../../Themes/Images';
+
+class Chekcout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const images = get(navigation, 'state.params.image', '');
+    const shared = get(navigation, 'state.params.shared', '');
+
+    return (
+      <View style={Styles.container}>
+        <Transition shared={shared} delay>
+          <Image
+            source={images}
+            style={StylesGlobal.sizeImg(120, 165)}
+          />
+        </Transition>
+        <Transition appear="bottom" delay>
+          <Image
+            source={Images.coffeeBag}
+            style={[StylesGlobal.sizeImg(139.1, 235.6), { position: 'absolute' }]}
+          />
+        </Transition>
+      </View>
+    )
+  }
+}
+
+export default Chekcout;
